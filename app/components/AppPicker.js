a624f473d34cbee12d6f63aadeb68e979fdafe39
GioKub <gkublashvili@unisens.ge>
2022-05-23 13:49:38 +0400

    extended AppPicker compononent by adding Modal windows

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -1,30 +1,44 @@
-import React from 'react'
-import { View, TextInput, StyleSheet, Platform } from 'react-native'
+import React, { useState } from 'react'
+import { View, TextInput, StyleSheet, Platform, TouchableWithoutFeedback, Modal, Button } from 'react-native'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 import AppText from './AppText'
+import Screen from './Screen'
 import defaultStyles from '../config/styles'
 
-function AppPicker({icon, placeholder, ...otherProps}){
-    return(
-        <View style={styles.container}>
-            {icon && <MaterialCommunityIcons 
-                name={icon}
-                size={30} 
-                color={defaultStyles.colors.medium} 
-                style={styles.icon}/>}
-            <AppText style={styles.text}>{placeholder}</AppText>
-            <MaterialCommunityIcons 
-                name="chevron-down" 
-                size={30} 
-                color={defaultStyles.colors.medium}
-                />
-        </View>
+
+function AppPicker({ icon, placeholder, ...otherProps }) {
+
+    const [modalVisible, setModalVisible] = useState(false)
+
+    return (
+        <>
+            <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+                <View style={styles.container}>
+                    {icon && <MaterialCommunityIcons
+                        name={icon}
+                        size={30}
+                        color={defaultStyles.colors.medium}
+                        style={styles.icon} />}
+                    <AppText style={styles.text}>{placeholder}</AppText>
+                    <MaterialCommunityIcons
+                        name="chevron-down"
+                        size={30}
+                        color={defaultStyles.colors.medium}
+                    />
+                </View>
+            </TouchableWithoutFeedback>
+            <Modal visible={modalVisible} animationType='slide'>
+                <Screen>
+                    <Button title='Close' onPress={() => setModalVisible(false)}></Button>
+                </Screen>
+            </Modal>
+        </>
     )
 }
 
 const styles = StyleSheet.create({
-    container:{
+    container: {
         backgroundColor: defaultStyles.colors.light,
         borderRadius: 25,
         flexDirection: "row",
@@ -32,10 +46,10 @@ const styles = StyleSheet.create({
         padding: 15,
         marginVertical: 10
     },
-    icon:{
+    icon: {
         marginRight: 10
     },
-    text:{
+    text: {
         flex: 1
     }
 })
