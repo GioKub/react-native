d86f61e3d250952faca8448eb39a380b784ce48b
GioKub <gkublashvili@unisens.ge>
2022-05-24 16:58:49 +0400

    implemented FormImagePicker componenet

--- App.js
+++ App.js
@@ -8,13 +8,7 @@ import ImageInputList from "./app/components/ImageInputList";
 export default function App() {
   const [imageUris, setImageUris] = useState([])
 
-  const handleAdd = uri =>{
-    setImageUris([...imageUris, uri])
-  }
 
-  const handleRemove = uri =>{
-    setImageUris(imageUris.filter(imageUri => imageUri!==uri))
-  }
 
   return (<Screen>
     <ImageInputList
