de0dd0686314c637080da3d7d01be4e1d4937c3a
GioKub <gkublashvili@unisens.ge>
2022-05-24 15:52:17 +0400

    implemented ImageInputList componenet to chose and display multiple images from gallery

--- App.js
+++ App.js
@@ -3,14 +3,24 @@ import * as ImagePicker from "expo-image-picker"
 import Screen from "./app/components/Screen";
 import { Button, Image} from "react-native";
 import ImageInput from "./app/components/ImageInput";
+import ImageInputList from "./app/components/ImageInputList";
 
 export default function App() {
-  const [imageUri, setImageUri] = useState()
+  const [imageUris, setImageUris] = useState([])
+
+  const handleAdd = uri =>{
+    setImageUris([...imageUris, uri])
+  }
+
+  const handleRemove = uri =>{
+    setImageUris(imageUris.filter(imageUri => imageUri!==uri))
+  }
 
   return (<Screen>
-    <ImageInput 
-    imageUri={imageUri}
-    onChangeImage={uri => setImageUri(uri)}
+    <ImageInputList
+    imageUris={imageUris}
+    onAddImage={handleAdd}
+    onRemoveImage={handleRemove}
     />
   </Screen>)
 }
